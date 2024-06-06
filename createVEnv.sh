e6c5789b7c96e42746db02810c6e15d670ef7306
missingusername <x7evaa@gmail.com>
2024-06-06 22:29:15 +0200

    virtual environment setup added

---
+++ createVEnv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# If virtualenv is not installed install it
+if ! command -v virtualenv &> /dev/null; then
+    echo "virtualenv not found. Installing..."
+    pip install virtualenv
+fi
+
+# Create the virtual environment
+virtualenv spatialVEnv
+
+# Activate / Go into virtual environment
+source spatialVEnv/Scripts/activate
+
+# install the notebook in the virtual environment
+pip install jupyter
+
+# Install the packages reading them line by line from the requirements file
+pip install -r requirements.txt 
+
+
+# Exit the virtual environment (in the command line that is)
+deactivate
